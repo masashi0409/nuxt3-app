8597699ffff0f58474154d045eaa8c3294fa7ef6
shimano <masashi.shimano0409@gmail.com>
2024-02-04 15:53:51 +0900

    assets

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
       <h1>Top Page</h1>
       <hr>
       <NuxtLink to="/price">Price</NuxtLink>
+      <div>
+        <img width="300" src="~/assets/cat.jpg" alt="cat">
+      </div>
     </div>
   </template>
   <script setup>
