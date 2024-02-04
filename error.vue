aeb10c6bba448cc4c99e97ccac6042c07c6b4e78
shimano <masashi.shimano0409@gmail.com>
2024-02-04 15:04:29 +0900

    errorページ

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-if="error.statusCode === 404">
+        <h1>error page</h1>
+        <p>404 error</p>
+        <p> {{ error.message }}</p>
+    </div>
+    <div v-if="error.statusCode !== 404">
+        <h1>error page</h1>
+        <div>status code:</div><div v-html="error.statusCode"></div>
+        <div>message:</div><div v-html="error.message"></div>
+        <div>{{ error }}</div>
+    </div>
+</template>
+<script setup>
+const error = useError() // https://zenn.dev/mm67/articles/nuxt3-use-error
+</script>
